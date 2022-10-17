0ea85e789e766ea5ae13d47c27d86261260fbf98
SeaLion-123 <silaskarabimanku@gmail.com>
2022-10-17 09:44:06 -0700

    My commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main-prints the words with proper grammar, but the outcome is a piece of
+ * art,
+ * Return: 0 if exited properly, no zero other wise
+ */
+int main(void)
+{
+     printf("with proper grammar, but the outcome is a piece of art,\n");
+     return (0);
+
+
