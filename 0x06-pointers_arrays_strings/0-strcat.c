adfe5274fab4e6e1ed86fb2ea2ef3d338af2308f
SeaLion-123 <silaskarabimanku@gmail.com>
2022-10-31 02:28:24 -0700

    My commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,13 +11,14 @@
 
 char *strcat(char *dest, const char *src)
 {
-	int index = 0, dest_len = 0;
+	int i, j;
 
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
-
+	for (i = 0; dest[i] != 0; i++)
+	{
+	}
+	for (j = 0; src[j] != 0; j++, i++)
+	{
+		dest[i] = src[j];
+	}
 	return (dest);
 }
